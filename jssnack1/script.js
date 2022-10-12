d8bc9d1170e8cf8395896a2943757fbb805570e7
SevenErol <lorenzo.maggi1998@gmail.com>
2022-10-12 11:39:33 +0200

    commit result visualized in DOM

--- jssnack1/script.js
+++ jssnack1/script.js
@@ -6,13 +6,18 @@ const numberOne = prompt("Inserisci il primo numero");
 
 const numberTwo = prompt ("Inserisci il secondo numero");
 
+const visualElement = document.querySelector("h1");
 
 if (numberOne > numberTwo) {
 
     console.log(numberOne);
 
+    visualElement.innerHTML = `Il numero maggiore è ${numberOne}`;
+
 } else if (numberOne < numberTwo) {
 
     console.log(numberTwo);
-    
+
+    visualElement.innerHTML = `Il numero maggiore è ${numberTwo}`;
+
 }
