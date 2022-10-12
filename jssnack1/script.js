ce0c7ddc1f656dc0715538be33fb666023653652
SevenErol <lorenzo.maggi1998@gmail.com>
2022-10-12 11:36:12 +0200

    commit biggest number veirified

---
+++ jssnack1/script.js
@@ -0,0 +1,18 @@
+// L’utente inserisce due numeri in successione, con due prompt.
+// Il software stampa il maggiore.
+
+
+const numberOne = prompt("Inserisci il primo numero");
+
+const numberTwo = prompt ("Inserisci il secondo numero");
+
+
+if (numberOne > numberTwo) {
+
+    console.log(numberOne);
+
+} else if (numberOne < numberTwo) {
+
+    console.log(numberTwo);
+    
+}
