518f49e8925d65ad069516ac897b9a2ee94185d4
SevenErol <lorenzo.maggi1998@gmail.com>
2022-10-12 12:29:47 +0200

    result visualized in DOM

--- jssnack3/script.js
+++ jssnack3/script.js
@@ -1,7 +1,11 @@
 // Il software deve chiedere per 10 volte all’utente di inserire un numero.
 // Il programma stampa la somma di tutti i numeri inseriti.
 
- let counter = 0;
+let counter = 0;
+
+const visualElement = document.querySelector("h1");
+
+
 
 for (let i = 0; i < 10; i++) {
 
@@ -11,3 +15,5 @@ for (let i = 0; i < 10; i++) {
 }
 
 console.log(counter);
+
+visualElement.innerHTML = `La somma dei numeri inseriti è: ${counter}`;
