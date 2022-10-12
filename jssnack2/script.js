8fc232962a627e10b8f03f1d0e3312c1e8281294
SevenErol <lorenzo.maggi1998@gmail.com>
2022-10-12 12:09:02 +0200

    commit result visualized in DOM

--- jssnack2/script.js
+++ jssnack2/script.js
@@ -6,16 +6,24 @@ const firstString = prompt("Inserisci la prima frase");
 
 const secondString = prompt("inserisci la seconda frase");
 
+const visualElement = document.querySelector("h1");
+
 if ( firstString.length > secondString.length) {
 
     console.log(firstString);
+
+    visualElement.innerHTML = `la frase più lunga è: ${firstString}`;
 
 } else if (firstString.length < secondString.length) {
 
     console.log(secondString);
 
+    visualElement.innerHTML = `la frase più lunga è: ${secondString}`;
+
 } else if (firstString.length === secondString.length) {
 
     console.log("Le due frasi sono lunghe uguali");
-    
+
+    visualElement.innerHTML = "Le due frasi sono lunghe uguali";
+
 }
